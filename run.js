e0cfc71640191f428d4e37a6496f97f52a719085
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-10-21 16:32:24 -0400

    trying to get env vars working

--- run.js
+++ run.js
@@ -1,11 +1,19 @@
 var login = require('facebook-chat-api');
-var config = require('./config')
+
+// set environment variables from hidden local file if not existing
+if (process.env['SET_ENV'] != 'true') {
+    var env_vars = require('./env_vars');
+    process.env['SET_ENV'] = env_vars.SET_ENV;
+    process.env['FB_EMAIL'] = env_vars.FB_EMAIL;
+    process.env['FB_PASSWORD'] = env_vars.FB_PASSWORD;
+    process.env['FAZ_ID'] = env_vars.FAZ_ID;
+}
 
 // Create simple echo bot
-login({email: config.FB_EMAIL, password: config.FB_PASSWORD}, function callback (err, api) {
+login({email: process.env['FB_EMAIL'], password: process.env['FB_PASSWORD']}, function callback (err, api) {
     if(err) return console.error(err);
 
     api.listen(function callback(err, message) {
-        api.sendMessage(message.body, config.id.faz);
+        api.sendMessage('[' + message.senderName + '] ' + message.body, process.env['FAZ_ID']);
     });
 });
