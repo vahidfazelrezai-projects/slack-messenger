f8b510489da1247e8a231076b4752a77ed4670c1
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2015-10-21 17:55:28 -0400

    logger

--- beaverbot.js
+++ beaverbot.js
@@ -1,22 +1,28 @@
 var login = require('facebook-chat-api');
 
-// set environment variables from hidden local file if not existing
-if (process.env['ENV_VERSION'] != '0') {
+// set environment variables from hidden 'env_vars.js' local file if possible
+try{
     var env_vars = require('./env_vars');
-    for (var env_var in env_vars) {
+    Object.keys(env_vars).forEach(function (env_var) {
         if (env_vars.hasOwnProperty(env_var)) {
             process.env[env_var] = env_vars[env_var];
         }
+    });
+    console.log('Using local env_vars file...')
+} catch(err){
+    if(err.code === 'MODULE_NOT_FOUND'){
+        console.log("Couldn't find local env_vars file, assuming environment already configured...")
     }
+    console.log(err);
 }
 
 // login and get api object
 login({email: process.env['FB_EMAIL'], password: process.env['FB_PASSWORD']}, function callback (err, api) {
     if(err) return console.error(err);
 
-    // logger
+    // logger relays all received messages into thread with id LOG_ID (except messages in log thread)
     api.listen(function callback(err, message) {
-        if (message.threadId != process.env['LOG_ID']) {
+        if (message.threadID != process.env['LOG_ID']) {
             api.sendMessage('[' + message.senderName + '] ' + message.body, process.env['LOG_ID']);
         }
     });
